e845ca69e4bdba990880ebe0f4d415141bdd0371
Rok Roskar <roskar@physik.uzh.ch>
2017-04-11 14:02:19 +0200

    added support for processing executor logs

--- spark_pyrest.py
+++ spark_pyrest.py
@@ -1,13 +1,14 @@
 import os
 import pandas as pd 
 import requests
+import re
 
 os.environ['http_proxy']=""
 
 class SparkPyRest(object): 
     """Basic class for handling Spark REST API queries"""
 
-    def __init__(self, host, port): 
+    def __init__(self, host, port=4040): 
         self.host = host
         self.port = port
         self.base_url = base_url = 'http://{host}:{port}/api/v1'.format(host=host, port=port) 
@@ -16,14 +17,64 @@ class SparkPyRest(object):
     def app(self): 
         return get_app(self.base_url)
 
+
     @property
     def stages(self): 
         return get_stages(self.base_url)
+
+
+    @property
+    def executors(self):
+        response = requests.get(self.base_url+'/applications/' + self.app + '/executors')
+        executors = response.json()
+        return executors
+
 
     def tasks(self, stageid):
         """Return the tasks for a given stageid"""
         return get_tasks(self.base_url, stageid)
 
+
+    def executor_log_bytes(self, executor_id):
+        """Find out how big the executor log file is"""
+        executors = self.executors
+
+        for executor in executors: 
+            if executor['id'] == executor_id:
+               break
+
+        log_url = executors[executor_id]
+        response = requests.get(executor['executorLogs']['stderr'].replace('logPage', 'log')+'&offset=0')
+        total_bytes = int(re.findall('\d+\sof\s(\d+)\sof', response.text)[0])
+        return total_bytes        
+
+
+    def executor_log(self, executor_id, nbytes='all'):
+        """Fetch the executors log; by default, the entire log is returned."""
+        total_bytes = self.executor_log_bytes(executor_id)
+
+        executors = self.executors
+
+        for executor in executors: 
+            if executor['id'] == executor_id:
+               break
+
+        if nbytes=='all':
+            nbytes = total_bytes
+
+        else: 
+            if not isinstance(nbytes,int): 
+                raise RuntimeError('nbytes must be an integer')
+
+        query = executor['executorLogs']['stderr'].replace('logPage', 'log')
+        query += '&offset=0'
+        query += '&byteLength={}'.format(nbytes)
+        response = requests.get(query)
+
+        return response.text
+
+
+# base public functions
 
 def get_app(base_url):
     """Get the app ID from the REST server"""
